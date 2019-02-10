0c3cd29a7ed254386818dab1a1077f82149d2bad
Vladimir Novoselskiy <vladimirnovoselskiy@vladimirs-macbook-pro.local>
2019-02-10 15:43:52 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Vladimir Novoselskiy on 2/10/19.
+//  Copyright © 2019 Vladimir Novoselskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
